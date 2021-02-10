f179d41146fd1f0aa09de4d72a59acd7c95bb1d9
jgarc594 <jgarc594@ucr.edu>
2021-02-10 12:03:40 -0800

    Added Unit Tests

--- test.cpp
+++ test.cpp
@@ -1,7 +1,72 @@
 #include "spreadsheet.hpp"
-
+#include "spreadsheet.cpp"
+#include "select.hpp"
+#include <sstream>
 #include "gtest/gtest.h"
+using namespace std;
 
+TEST(test, Select_ContainsTest) {
+    stringstream ss;
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+
+    sheet.set_selection(new Select_Contains(&sheet,"Last","Dole"));
+    sheet.print_selection(ss);
+
+    EXPECT_EQ(ss.str(), "Diane Dole 20 computer engineering\nDavid Dole 22 electrical engineering\nDominick Dole 22 communications");
+}
+
+TEST(test, Select_ContainsDoesNotContainTest) {
+    stringstream ss;
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+
+    sheet.set_selection(new Select_Contains(&sheet,"Last","Smith"));
+    sheet.print_selection(ss);
+
+    EXPECT_EQ(ss.str(), "");
+}
+
+TEST(test, Select_NotTest) {
+    stringstream ss;
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+
+    sheet.set_selection(new Select_Contains(&sheet,"Last","Smith"));
+    sheet.print_selection(ss);
+
+    EXPECT_EQ(ss.str(), "");
+}
+
+TEST(test, Select_AndTest) {
+    stringstream ss;
+    Spreadsheet sheet;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+
+    sheet.print_selection(ss);
+
+    EXPECT_EQ(ss.str(), "Sarah Summers 21 computer science");
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
